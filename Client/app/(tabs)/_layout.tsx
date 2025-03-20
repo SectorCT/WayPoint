d47db31cc5e0a21acbfa1e4da3ae0aea3ee58295
Svetoslav47 <svetoslav2806@gmail.com>
2025-03-20 17:16:35 +0200

    tabs navigation

---
+++ Client/app/(tabs)/_layout.tsx
@@ -0,0 +1,37 @@
+import { Tabs } from "expo-router";
+import { MaterialIcons } from '@expo/vector-icons';
+import { useTheme } from '@context/ThemeContext';
+
+export default function TabLayout() {
+  const { theme } = useTheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: theme.color.darkPrimary,
+        tabBarInactiveTintColor: 'rgba(0, 0, 0, 0.6)',
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="addPackage"
+        options={{
+          title: "Add Package",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="add-box" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen name="styles/homeStyles" options={{ href: null }} />
+    </Tabs>
+  );
+} 
