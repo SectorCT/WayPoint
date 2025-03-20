e2218042204246ea84c803d2e4cc9866eb2e3a9c
Antonio Gotev <antong318@gmail.com>
2025-03-20 17:30:12 +0200

    Added new Package functionalities
    getAllPackages()
    deletePackage()
    bulkCreatePackages()

--- server/authentication/views.py
+++ server/authentication/views.py
@@ -1,7 +1,7 @@
-from rest_framework.decorators import api_view
 from rest_framework.views import APIView    
 from rest_framework.response import Response
 from rest_framework import status, views
+from django.http import JsonResponse
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
--- server/delivery/views.py
+++ server/delivery/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializer import PackageSerializer
 from rest_framework import status
+from .models import Package
+from django.http import JsonResponse
 
 class createPackage(APIView):
     def post(self, request):
@@ -12,3 +14,44 @@ class createPackage(APIView):
 
         error_messages = " ".join([" ".join(messages) for messages in serializer.errors.values()])
         return Response({"detail": error_messages}, status=status.HTTP_400_BAD_REQUEST)
+
+class bulkCreatePackages(APIView):
+    def post(self, request):
+        packages_data = request.data.get('packages', [])
+        if not isinstance(packages_data, list):
+            return Response({"detail": "Invalid data format. Expecting a list of packages."}, status=status.HTTP_400_BAD_REQUEST)
+
+        created_packages = []
+        errors = []
+
+        for idx, package_data in enumerate(packages_data):
+            serializer = PackageSerializer(data=package_data)
+            if serializer.is_valid():
+                serializer.save()
+                created_packages.append(serializer.data)
+            else:
+                error_messages = " ".join([" ".join(messages) for messages in serializer.errors.values()])
+                errors.append({"index": idx, "errors": error_messages})
+
+        response = {
+            "created_packages": created_packages,
+            "errors": errors,
+        }
+
+        status_code = status.HTTP_207_MULTI_STATUS if errors else status.HTTP_201_CREATED
+        return Response(response, status=status_code)
+    
+class getAllPackages(APIView):
+    def get(self, request):
+        packages = Package.objects.all()
+        serializer = PackageSerializer(packages, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class deletePackage(APIView):
+    def delete(self, request, id):
+        try:
+            package = Package.objects.get(id=id)
+            package.delete()
+            return Response({"detail": f"Package with ID {id} deleted."}, status=status.HTTP_200_OK)
+        except Package.DoesNotExist:
+            return JsonResponse({"detail": f"Package with ID {id} not found."}, status=404)
